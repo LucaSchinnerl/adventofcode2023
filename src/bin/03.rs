5f6e2ef317f5b902cffeb7037b069bec5e7031f6
luca-schi <schinnerl.luca@gmail.com>
2023-12-03 17:47:14 +0100

    part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,25 +1,38 @@
 use regex::Regex;
+use std::collections::HashMap;
 
-fn main() {
-    let input = include_str!("../../inputs/03.in");
+#[aoc::main(03)]
+fn main(input: &str) -> (u32, u32) {
+    let r = solution(input);
 
-    let r = part1(input);
-
-    println!("Result: {}", r);
+    (r.0, r.1)
 }
 
-fn part1(input: &str) -> u32 {
+fn solution(input: &str) -> (u32, u32) {
     let ranges = get_digit_ranges(input);
 
-    let map = input.lines().map(|l| l.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+    let map = input
+        .lines()
+        .map(|l| l.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
 
-    let mut res = 0;
-    for r in ranges {
-        if check_adjacent(&map, &r){
-            res += r.number as u32;
+    let mut total_points = HashMap::new();
+
+    ranges.iter().for_each(|r| {
+        add_adjecient(&map, r, &mut total_points);
+    });
+
+    let mut p1 = 0;
+    let mut p2 = 0;
+
+    total_points.iter().for_each(|(_, v)| {
+        p1 += v.iter().sum::<u32>();
+        if v.len() > 1 {
+            p2 += v.iter().product::<u32>();
         }
-    }
-    res
+    });
+
+    (p1, p2)
 }
 
 #[derive(Debug)]
@@ -28,7 +41,13 @@ struct NumberRange {
     x_end: usize,
     y_start: usize,
     y_end: usize,
-    number: u32
+    number: u32,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy, Hash, Eq)]
+struct Point {
+    x: usize,
+    y: usize,
 }
 
 fn get_digit_ranges(input: &str) -> Vec<NumberRange> {
@@ -38,9 +57,7 @@ fn get_digit_ranges(input: &str) -> Vec<NumberRange> {
     for (y, line) in input.lines().enumerate() {
         for mat in re.find_iter(line) {
             let mut x_start = mat.start();
-            if x_start != 0{
-                x_start -= 1;
-            }
+            x_start = x_start.saturating_sub(1);
             let mut x_end = mat.end() - 1;
             if x_end != line.len() - 1 {
                 x_end += 1;
@@ -49,34 +66,41 @@ fn get_digit_ranges(input: &str) -> Vec<NumberRange> {
             let mut y_start = y;
             let mut y_end = y;
 
-            if y_start != 0 {
-                y_start -= 1;
-            }
-
+            y_start = y_start.saturating_sub(1);
 
             if y_end != line.len() - 1 {
                 y_end += 1;
             }
 
-            result.push(
-                NumberRange { x_start: x_start, x_end: x_end, y_start: y_start, y_end: y_end, number: mat.as_str().parse::<u32>().unwrap() }
-            );
-
+            result.push(NumberRange {
+                x_start,
+                x_end,
+                y_start,
+                y_end,
+                number: mat.as_str().parse::<u32>().unwrap(),
+            });
         }
     }
     result
 }
 
-fn check_adjacent(map: &Vec<Vec<char>>, r: &NumberRange) -> bool {
-
+fn add_adjecient(
+    map: &Vec<Vec<char>>,
+    r: &NumberRange,
+    total_points: &mut HashMap<Point, Vec<u32>>,
+) {
     for y in r.y_start..=r.y_end {
         for x in r.x_start..=r.x_end {
-            if !map[y][x].is_numeric() && map[y][x] != '.'  {
-                return true;
+            if !map[y][x].is_numeric() && map[y][x] != '.' {
+                let temp_point = Point { x, y };
+                total_points
+                    .entry(temp_point)
+                    .or_insert(Vec::new())
+                    .push(r.number);
+                return ;
             }
         }
     }
-    false
 }
 
 #[cfg(test)]
@@ -84,7 +108,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part1() {
+    fn test_sol() {
         let input = "467..114..
 ...*......
 ..35..633.
@@ -95,6 +119,6 @@ mod tests {
 ......755.
 ...$.*....
 .664.598..";
-        assert_eq!(part1(input), 4361);
+        assert_eq!(solution(input), (4361, 467835));
     }
 }
