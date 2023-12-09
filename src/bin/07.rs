4070c85c373a92125aa52c10fcaeae07eee9c9bc
luca-schi <schinnerl.luca@gmail.com>
2023-12-09 16:14:03 +0100

    part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -2,17 +2,13 @@ use itertools::Itertools;
 use std::usize;
 
 #[aoc::main(07)]
-fn main(input: &str) -> (u64, u64) {
-    println!("{:?}", solution(input));
-    println!("{:?}", solution(input));
-
-    (0, 0)
+fn main(input: &str) -> (u32, u32) {
+    (solution(input, false), solution(input, true))
 }
 
-fn solution(input: &str) -> u32 {
+fn solution(input: &str, part2: bool) -> u32 {
     let mut sorted_hands = input.lines().map(Hand::new).collect::<Vec<Hand>>();
-
-    sorted_hands.sort_by(complex_criterion);
+    sorted_hands.sort_by(|a, b| complex_criterion(a, b, part2));
     sorted_hands
         .iter()
         .enumerate()
@@ -35,9 +31,25 @@ impl Hand {
         }
     }
 
-    fn rank(&self) -> u32 {
-        let combination: Vec<usize> = self
-            .cards
+    fn rank(&self, part2: bool) -> u32 {
+        let mut cards = self.cards.clone();
+        if part2 {
+            if self.cards == "JJJJJ" {
+                cards = "AAAAA".to_string()
+            } else {
+                let most_common_char = sort_string_with_custom_values(
+                    self.cards.chars().filter(|&c| c != 'J').collect::<String>(),
+                    part2,
+                )
+                .chars()
+                .max_by_key(|c| self.cards.chars().filter(|&x| x == *c).count())
+                .unwrap();
+
+                cards = self.cards.replace('J', &most_common_char.to_string());
+            }
+        }
+
+        let combination: Vec<usize> = cards
             .chars()
             .sorted()
             .group_by(|c| *c)
@@ -59,10 +71,23 @@ impl Hand {
     }
 }
 
-fn get_value(c: char) -> u32 {
+fn sort_string_with_custom_values(s: String, part2: bool) -> String {
+    let mut chars: Vec<char> = s.chars().collect();
+    chars.sort_by_key(|a| get_value(*a, part2));
+    //chars.reverse();
+    chars.into_iter().collect()
+}
+
+fn get_value(c: char, part2: bool) -> u32 {
     match c {
         'T' => 10,
-        'J' => 11,
+        'J' => {
+            if part2 {
+                1
+            } else {
+                11
+            }
+        }
         'Q' => 12,
         'K' => 13,
         'A' => 14,
@@ -70,15 +95,15 @@ fn get_value(c: char) -> u32 {
     }
 }
 
-fn complex_criterion(a: &Hand, b: &Hand) -> std::cmp::Ordering {
-    match a.rank().cmp(&b.rank()) {
+fn complex_criterion(a: &Hand, b: &Hand, part2: bool) -> std::cmp::Ordering {
+    match a.rank(part2).cmp(&b.rank(part2)) {
         std::cmp::Ordering::Less => std::cmp::Ordering::Less,
         std::cmp::Ordering::Greater => std::cmp::Ordering::Greater,
         std::cmp::Ordering::Equal => a
             .cards
             .chars()
             .zip(b.cards.chars())
-            .map(|(a, b)| get_value(a).cmp(&get_value(b)))
+            .map(|(a, b)| get_value(a, part2).cmp(&get_value(b, part2)))
             .find(|&ordering| ordering != std::cmp::Ordering::Equal)
             .unwrap_or(std::cmp::Ordering::Equal),
     }
@@ -95,6 +120,8 @@ T55J5 684
 KK677 28
 KTJJT 220
 QQQJA 483";
-        assert_eq!(solution(input), 6440);
+        assert_eq!(solution(input, false), 6440);
+
+        assert_eq!(solution(input, true), 5905);
     }
 }
