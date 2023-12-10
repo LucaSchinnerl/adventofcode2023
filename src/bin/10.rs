c4b6fcea3a6d38f5dd13648fa494d31000fc7d73
luca-schi <schinnerl.luca@gmail.com>
2023-12-10 23:32:01 +0100

    Simplified part 1

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,77 +1,65 @@
-use petgraph::graph::DiGraph;
-use petgraph::prelude::*;
-use petgraph::visit::Bfs;
-use petgraph::Direction;
-use std::collections::HashMap;
-
-use std::usize;
-
 #[aoc::main(10)]
 fn main(input: &str) -> (u32, u32) {
-    (solution(input), 0)
+    let char_map = input
+        .lines()
+        .map(|row| row.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+    let (steps, _map) = solution(&char_map);
+
+    (steps, 0)
 }
 
-fn solution(input: &str) -> u32 {
-    let (graph, starting_node) = create_node_edge_pairs(input);
+fn solution(map: &[Vec<char>]) -> (u32, Vec<Vec<u8>>) {
+    let mut map_path: Vec<Vec<u8>> = map
+        .iter()
+        .map(|row| row.iter().map(|_| 0).collect())
+        .collect();
 
-    let mut bfs = Bfs::new(&graph, starting_node);
-    let mut distance = 0;
+    let start_position = find_start_position(map);
+    map_path[start_position.1][start_position.0] = 1;
+    let mut prev = start_position;
+    let mut current_position = get_starting_direction(start_position, map);
+    let mut steps = 1;
 
-    while let Some(_next_node) = bfs.next(&graph) {
-        distance += 1;
+    loop {
+        map_path[current_position.1][current_position.0] = 1;
+        let (x, y) = current_position;
+        let c = map[y][x];
+        let connections = get_connections(c, x, y);
+        let next = filter_next_position(connections, &prev);
+        prev = current_position;
+        current_position = next;
+        steps += 1;
+        if current_position == start_position {
+            break;
+        }
     }
-    distance / 2
+    (steps / 2, map_path)
 }
 
-fn create_node_edge_pairs(input: &str) -> (DiGraph<(usize, usize), ()>, NodeIndex) {
-    let x_bound = input.lines().next().unwrap().len();
-    let y_bound = input.lines().count();
-    let mut graph: DiGraph<(usize, usize), ()> = DiGraph::new();
-    let mut node_indices = HashMap::new();
-    let mut starting_node: NodeIndex = NodeIndex::new(0);
-    input.lines().enumerate().for_each(|(y, line)| {
-        line.chars().enumerate().for_each(|(x, c)| {
-            if c == '.' {
-                return;
-            }
-            let index = graph.add_node((x, y));
-            if c == 'S' {
-                starting_node = index;
-            }
-            node_indices.insert((x, y), index);
-        });
-    });
+fn find_start_position(map: &[Vec<char>]) -> (usize, usize) {
+    map.iter()
+        .enumerate()
+        .find_map(|(y, line)| {
+            line.iter()
+                .enumerate()
+                .find_map(|(x, c)| if *c == 'S' { Some((x, y)) } else { None })
+        })
+        .unwrap()
+}
 
-    input.lines().enumerate().for_each(|(y, line)| {
-        line.chars().enumerate().for_each(|(x, c)| {
-            if c == '.' {
-                return;
-            }
-            let connections = get_connections(c, x, y);
-            connections.iter().for_each(|(x_new, y_new)| {
-                if check_bounds(*x_new, *y_new, x_bound, y_bound) {
-                    let target_index = node_indices.get(&(*x_new, *y_new));
-                    // If target_index is an error print the index and the coordinates
-                    let target_index = match target_index {
-                        Some(index) => *index,
-                        None => {
-                            return;
-                        }
-                    };
-                    graph.add_edge(node_indices[&(x, y)], target_index, ());
-                }
-            });
-        });
-    });
-
-    let pointing_nodes = graph
-        .neighbors_directed(starting_node, Direction::Incoming)
-        .collect::<Vec<NodeIndex>>();
-
-    pointing_nodes.iter().for_each(|node_index| {
-        graph.add_edge(starting_node, *node_index, ());
-    });
-    (graph, starting_node)
+fn get_starting_direction(starting_position: (usize, usize), map: &[Vec<char>]) -> (usize, usize) {
+    let (x, y) = starting_position;
+    for direction in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
+        let (dx, dy) = direction;
+        let next_position = ((x as i32 + dx) as usize, (y as i32 + dy) as usize);
+        let c = map[next_position.1][next_position.0];
+        let connections = get_connections(c, next_position.0, next_position.1);
+        if connections.contains(&starting_position) {
+            return next_position;
+        }
+    }
+    panic!("Invalid starting position")
 }
 
 fn get_connections(c: char, x: usize, y: usize) -> Vec<(usize, usize)> {
@@ -82,8 +70,6 @@ fn get_connections(c: char, x: usize, y: usize) -> Vec<(usize, usize)> {
         'J' => vec![(x, y.saturating_sub(1)), (x.saturating_sub(1), y)],
         '7' => vec![(x, y + 1), (x.saturating_sub(1), y)],
         'F' => vec![(x, y + 1), (x + 1, y)],
-        '.' => vec![],
-        'S' => vec![],
         _ => {
             println!("{}", c);
             panic!("Invalid character")
@@ -91,42 +77,6 @@ fn get_connections(c: char, x: usize, y: usize) -> Vec<(usize, usize)> {
     }
 }
 
-fn check_bounds(x: usize, y: usize, x_bound: usize, y_bound: usize) -> bool {
-    x < x_bound && y < y_bound
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_get_connections() {
-        assert_eq!(get_connections('|', 10, 10), vec![(10, 11), (10, 9)]);
-        assert_eq!(get_connections('-', 10, 10), vec![(11, 10), (9, 10)]);
-        assert_eq!(get_connections('L', 10, 10), vec![(10, 9), (11, 10)]);
-        assert_eq!(get_connections('J', 10, 10), vec![(10, 9), (9, 10)]);
-        assert_eq!(get_connections('7', 10, 10), vec![(10, 11), (9, 10)]);
-        assert_eq!(get_connections('F', 10, 10), vec![(10, 11), (11, 10)]);
-        assert_eq!(get_connections('.', 10, 10), vec![]);
-    }
-
-    #[test]
-    fn test_first_example() {
-        let input = ".....
-.S-7.
-.|.|.
-.L-J.
-.....";
-        assert_eq!(solution(input), 4);
-    }
-
-    #[test]
-    fn test_second_example() {
-        let input = "..F7.
-.FJ|.
-SJ.L7
-|F--J
-LJ...";
-        assert_eq!(solution(input), 8);
-    }
+fn filter_next_position(next: Vec<(usize, usize)>, prev: &(usize, usize)) -> (usize, usize) {
+    next.into_iter().find(|(x, y)| &(*x, *y) != prev).unwrap()
 }
