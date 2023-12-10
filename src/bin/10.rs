67058f938fe10d00fef0e18f19f140d109fcbe38
luca-schi <schinnerl.luca@gmail.com>
2023-12-11 00:10:34 +0100

    Very slow part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,40 +1,59 @@
+use geo::Contains;
+use geo::{point, LineString, Polygon};
+
 #[aoc::main(10)]
 fn main(input: &str) -> (u32, u32) {
     let char_map = input
         .lines()
         .map(|row| row.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
-    let (steps, _map) = solution(&char_map);
 
-    (steps, 0)
+    let (steps, path) = solution(&char_map);
+
+    let ls = LineString::from(path);
+
+    let polygon = Polygon::new(ls, vec![]);
+    let mut area = 0;
+    char_map.iter().enumerate().for_each(|(y, line)| {
+        line.iter().enumerate().for_each(|(x, _)| {
+            if polygon.contains(&point!(x: x as f64, y: y as f64)) {
+                area += 1;
+            }
+        })
+    });
+
+    (steps, area)
 }
 
-fn solution(map: &[Vec<char>]) -> (u32, Vec<Vec<u8>>) {
-    let mut map_path: Vec<Vec<u8>> = map
-        .iter()
-        .map(|row| row.iter().map(|_| 0).collect())
-        .collect();
+fn solution(map: &[Vec<char>]) -> (u32, Vec<(f64, f64)>) {
+    let mut coordinate_path = Vec::new();
 
     let start_position = find_start_position(map);
-    map_path[start_position.1][start_position.0] = 1;
+    coordinate_path.push(start_position);
     let mut prev = start_position;
     let mut current_position = get_starting_direction(start_position, map);
     let mut steps = 1;
 
     loop {
-        map_path[current_position.1][current_position.0] = 1;
-        let (x, y) = current_position;
-        let c = map[y][x];
-        let connections = get_connections(c, x, y);
+        coordinate_path.push(current_position);
+        if current_position == start_position {
+            break;
+        }
+
+        let c = map[current_position.1][current_position.0];
+        let connections = get_connections(c, current_position.0, current_position.1);
         let next = filter_next_position(connections, &prev);
         prev = current_position;
         current_position = next;
         steps += 1;
-        if current_position == start_position {
-            break;
-        }
     }
-    (steps / 2, map_path)
+    (
+        steps / 2,
+        coordinate_path
+            .iter()
+            .map(|(x, y)| (*x as f64, *y as f64))
+            .collect(),
+    )
 }
 
 fn find_start_position(map: &[Vec<char>]) -> (usize, usize) {
