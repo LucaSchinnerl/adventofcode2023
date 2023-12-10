14ab788c751b9b4edb0cf62eebdedb31fb81d44e
luca-schi <schinnerl.luca@gmail.com>
2023-12-10 16:38:40 +0100

    part 1

---
+++ src/bin/04.rs
@@ -0,0 +1,42 @@
+#[aoc::main(04)]
+fn main(input: &str) -> (u32, u32) {
+    (solution(input), 0)
+}
+
+fn solution(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let (cards, numbers) = line.split_once(" | ").unwrap();
+            let winning = cards
+                .split(": ")
+                .nth(1)
+                .unwrap()
+                .split_whitespace()
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>();
+            let pow = numbers
+                .split_whitespace()
+                .map(|x| x.parse::<u32>().unwrap())
+                .fold(0, |acc, x| if winning.contains(&x) { acc + 1 } else { acc })
+                - 1;
+            2_u32.pow(pow)
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solution() {
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        assert_eq!(solution(input), 13);
+    }
+}
